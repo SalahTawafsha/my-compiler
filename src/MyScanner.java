07c4e3c868e3efeea39152e03a534afe041f66b9
salah tawafsha <salaht321@gmail.com>
2023-12-25 23:34:38 +0200

    create classes that needed

--- src/MyScanner.java
+++ src/MyScanner.java
@@ -7,22 +7,22 @@ public class MyScanner {
     private final static HashSet<String> reservedWords;
 
     static {
-        terminals = initializeSet(".","module", ";", "begin", "end","", "const","var",
-                "(",",",")","integer","real","char","procedure", ":=","+","-","*","/","mod",
-                "div","readint","readreal ","readchar", "readln","writeint","writereal",
-                "writechar", "writeln","if","then", "end", "elseif", "else", "while", "do",
-                "loop", "until", "exit", "call", "=","|=","<","<=",">",">=",
-                "0","1","2","3","4","5","6","7","8","9",
-                "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-                "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+        terminals = initializeSet(".", "module", ";", "begin", "end", "", "const", "var",
+                "(", ",", ")", "integer", "real", "char", "procedure", ":=", "+", "-", "*", "/", "mod",
+                "div", "readint", "readreal ", "readchar", "readln", "writeint", "writereal",
+                "writechar", "writeln", "if", "then", "end", "elseif", "else", "while", "do",
+                "loop", "until", "exit", "call", "=", "|=", "<", "<=", ">", ">=",
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 
-        nonTerminals = initializeSet("module-decl","module-heading",
-                "block","declarations","const-decl","const-list","var-decl","var-list",
-                "var-item","name-list","data-type","procedure-decl","procedure-heading",
-                "stmt-list","statement","ass-stmt","exp","term","factor","add-oper",
-                "mul-oper","read-stmt","write-stmt","write-list","write-item","if-stmt",
-                "elseif-part","else-part", "while-stmt","repeat-stmt","exit-stmt","call-stmt",
-                "condition","name-value","relational-oper","name","value","integer-value","real-value");
+        nonTerminals = initializeSet("module-decl", "module-heading",
+                "block", "declarations", "const-decl", "const-list", "var-decl", "var-list",
+                "var-item", "name-list", "data-type", "procedure-decl", "procedure-heading",
+                "stmt-list", "statement", "ass-stmt", "exp", "term", "factor", "add-oper",
+                "mul-oper", "read-stmt", "write-stmt", "write-list", "write-item", "if-stmt",
+                "elseif-part", "else-part", "while-stmt", "repeat-stmt", "exit-stmt", "call-stmt",
+                "condition", "name-value", "relational-oper", "name", "value", "integer-value", "real-value");
 
         reservedWords = initializeSet("module", "begin", "end", "const",
                 "var", "integer", "real", "char", "procedure", "mod", "div",
@@ -35,5 +35,9 @@ public class MyScanner {
         HashSet<String> set = new HashSet<>(values.length);
         Collections.addAll(set, values);
         return set;
+    }
+
+    public String nextToken() {
+        return "";
     }
 }
